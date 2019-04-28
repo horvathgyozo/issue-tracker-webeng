d1969af8b8c82943f5165f21d3b68bf0129e9945
Győző Horváth <gyozo.horvath@inf.elte.hu>
2019-04-28 23:20:21 +0200

    Multiple entry point configuration

--- src/main/java/hu/elte/IssueTracker/security/MultipleEntryPointsSecurityConfig.java
+++ src/main/java/hu/elte/IssueTracker/security/MultipleEntryPointsSecurityConfig.java
@@ -3,6 +3,7 @@ package hu.elte.IssueTracker.security;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -15,36 +16,11 @@ import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 @Configuration
 @EnableWebSecurity
 @EnableGlobalMethodSecurity(securedEnabled = true)
-public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
-
-    @Override
-    protected void configure(HttpSecurity http) throws Exception {
-        http
-            .authorizeRequests()
-                .antMatchers("/", "/h2/**", "/register").permitAll()
-//                .antMatchers("/api/**").permitAll()
-                .anyRequest().authenticated()
-                .and()
-            .formLogin()
-                .loginPage("/login")
-                .permitAll()
-                .and()
-            .logout()
-                .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
-                .logoutSuccessUrl("/")
-                .and()
-            .csrf()         // important!
-                .ignoringAntMatchers("/h2/**")
-                .and()
-            .headers()
-                .frameOptions().disable()  // important!
-                .and()
-            .httpBasic();
-    }
+public class MultipleEntryPointsSecurityConfig {
 
     @Autowired
     private UserDetailsService userDetailsService;
-    
+
     @Autowired
     protected void configureGlobal(AuthenticationManagerBuilder auth) throws Exception {
         auth.userDetailsService(userDetailsService).passwordEncoder(passwordEncoder());
@@ -57,4 +33,48 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
     public BCryptPasswordEncoder passwordEncoder() {
         return new BCryptPasswordEncoder();
     }
+
+    @Configuration
+    @Order(1)
+    public static class RestWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/api/**")
+                    .authorizeRequests()
+                    .anyRequest().authenticated()
+                    .and()
+                    .httpBasic();
+        }
+
+    }
+    
+    @Configuration
+    @Order(2)
+    public static class ApplWebSecurityConfig extends WebSecurityConfigurerAdapter {
+
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.antMatcher("/**")
+                    .authorizeRequests()
+                    .antMatchers("/", "/h2/**", "/register").permitAll()
+                    .anyRequest().authenticated()
+                    .and()
+                    .formLogin()
+                    .loginPage("/login")
+                    .permitAll()
+                    .and()
+                    .logout()
+                    .logoutRequestMatcher(new AntPathRequestMatcher("/logout"))
+                    .logoutSuccessUrl("/")
+                    .and()
+                    .csrf() // important!
+                    .ignoringAntMatchers("/h2/**")
+                    .and()
+                    .headers()
+                    .frameOptions().disable(); // important!
+        }
+
+    }
+    
 }
